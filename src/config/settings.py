217629b6b8ea779d3784418f3679e174b67925e9
Zhaochen Jiang <zhaochen.j@outlook.com>
2025-03-30 19:04:20 -0400

    Update data ranges in settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -9,6 +9,8 @@ DATA_PATHS = {
 }
 
 # Default date ranges
+
+'''
 DATE_RANGES = {
     "training": {
         "start_date": date(2020, 1, 1),
@@ -21,6 +23,14 @@ DATE_RANGES = {
     "validation": {
         "start_date": date(2024, 1, 1),
         "end_date": date.today(),
+    }
+}  
+'''
+
+DATE_RANGES = {
+    "test_period": {
+        "start_date": date(2023, 11, 1),
+        "end_date": date(2025, 3, 31),
     }
 }
 
